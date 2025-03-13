d321dddb596606d120c6d0e533c28bc877d57eb9
Hamdalah5 <doli@smuttech.co.id>
2025-03-13 14:33:50 +0700

    #Note API Database ( SQLite )

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,7 +11,7 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $user = User::create([
-            'name' => $request->name,
+            'username' => $request->username,
             'email' => $request->email,
             'password' => Hash::make($request->password)
         ]);
